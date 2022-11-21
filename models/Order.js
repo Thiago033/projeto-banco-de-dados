98cdc2d2fb18039e15a87b75d044c263defa2b49
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-11-21 13:01:42 -0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,42 +1,33 @@
 const db = require("../config/db");
 
 class Order {
-    constructor(cod_pedido ,email, isbn, valor_total, quantidade, situacao) {
-        this.cod_pedido = cod_pedido;
-        this.email = email;
-        this.isbn = isbn;
+    constructor(id_usuario, valor_total, situacao, tipo) {
+        this.id_usuario = id_usuario;
         this.valor_total = valor_total;
-        this.quantidade = quantidade;
         this.situacao = situacao;
-    }
-
-    //find all orders for user email
-    static findAllOrdersById(email){
-        let sql = `SELECT * FROM pedido WHERE email = '${email}';`;
-
-        return db.execute(sql);
-    }
- 
-    static findOrderById(id) {
-
-        let sql = `SELECT * FROM pedido WHERE cod_pedido = ${id};`;
-
-        return db.execute(sql);
+        this.tipo = tipo;
     }
 
     //save on database
-    async save() {
+    async saveOrderOnDatabase() {
 
-        let sql = `INSERT INTO pedido VALUES(NULL, '${this.email}', '${this.isbn}', ${this.valor_total}, ${this.quantidade}, '${this.situacao}')`;
+        let sql = `INSERT INTO pedido(id_usuario, valor_total, situacao, tipo) VALUES(${this.id_usuario}, ${this.valor_total}, '${this.situacao}', ${this.tipo});`;
         
         const [newOrder, _] = await db.execute(sql);
         
         return newOrder;
     }
 
-    //payment
-    static payment(id) {
-        let sql = `UPDATE pedido SET situacao = 'Aprovado' WHERE cod_pedido = ${id};`
+    //find all orders for user id
+    static findAllOrdersById(id){
+        let sql = `SELECT * FROM pedido WHERE id_usuario = ${id};`;
+
+        return db.execute(sql);
+    }
+
+     //find order by id
+     static findOrderById(id){
+        let sql = `SELECT * FROM pedido WHERE cod_pedido = ${id};`;
 
         return db.execute(sql);
     }
