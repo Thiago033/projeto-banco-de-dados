6caa18498b7414484309dde8860e438b68a44c36
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2022-12-03 13:32:38 -0300

    Update publishers.js

--- controllers/publishers.js
+++ controllers/publishers.js
@@ -1,9 +1,9 @@
-const Publishers = require('../models/Publishers');
+const Publishers = require('../models/Publisher');
 
 exports.showAllPublishers = async (req, res, next) => {
     try {
         const [publishers, _] = await Publishers.findAllPublishers();
-        res.render('publishers-page/publishers-index', {publishers});
+        res.render('publishers/index', {publishers});
         
     } catch (error) {
         console.log("error");
@@ -13,10 +13,10 @@ exports.showAllPublishers = async (req, res, next) => {
 
 exports.showPublisherById = async (req, res, next) => {
     try {
-        const [publishers] = await Publishers.findAllPublishers();
+        const [publishers] = await Publishers.findPublisherById(req.params.id);
         const [books, _] = await Publishers.findAllBooksByPublisherId(req.params.id);
     
-        res.render('publishers-page/publishers-show', {books, publishers: publishers[0]});
+        res.render('publishers/show', {books, publishers: publishers[0]});
         
     } catch (error) {
         console.log("error");
